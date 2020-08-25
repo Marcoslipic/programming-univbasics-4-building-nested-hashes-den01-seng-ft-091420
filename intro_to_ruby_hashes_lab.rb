6ed0cbef1aabeb3cb9034f09dd96d942a64367ff
Marcos Lipic <mjlipic.37@gmail.com>
2020-08-25 17:13:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,9 @@ def monopoly_with_second_tier
   
   hash = {:railroads => {:pieces => 4,
     :rent_in_dollars => {
-      :one_piece_owned => 4
+      :one_piece_owned => 25,
+      :two_piece_owned => 50,
+      :three_piece
   }}
 
 end

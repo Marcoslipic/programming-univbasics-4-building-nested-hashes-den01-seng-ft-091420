38eb6a30345754aabac509ab20456bde6b98d244
Marcos Lipic <mjlipic.37@gmail.com>
2020-08-25 17:24:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -35,8 +35,10 @@ def monopoly_with_third_tier
      :names => {
        :reading_railroad => {
        },
-       :pennsylvania_railroad => {}
+       :pennsylvania_railroad => {},
+       :b_and_o_railroad => {}
      }
+     
    }}
 
 end

6a4acc512fdf6eb39cb20d048eaa99a02884f548
Marcos Lipic <mjlipic.37@gmail.com>
2020-08-25 17:20:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,7 +12,7 @@ def monopoly_with_second_tier
   # complexity.
   
   hash = {:railroads => {:pieces => 4,
-    :rent_in_dollars => {}
+    :rent_in_dollars => {},
     :names => {}
   }}
 
@@ -29,7 +29,7 @@ def monopoly_with_third_tier
       :two_pieces_owned => 50,
       :three_pieces_owned => 100,
       :four_pieces_owned => 200
-  }
+  },
      :names => {}
    }}
 
